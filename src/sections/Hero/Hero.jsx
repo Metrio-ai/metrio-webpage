a856782af370f955632f724bca5e439c10332c3b
sierrapablo <pablosierralorente@outlook.es>
2024-11-15 21:07:08 +0100

    add classNames to order animations

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,10 +6,10 @@ function Hero() {
     return (
       <section className="heroContainer">
         <div className="heroOverlay"></div>
-        <h1 className="heroTitle">Conectar</h1>
-        <h1 className="heroTitle">Analizar</h1>
-        <h1 className="heroTitle">Impulsar</h1>
-        <a className="heroTitle buttonShowMore" href="#about">Descubre cómo</a>
+        <h1 className="heroTitle conectar">Conectar</h1>
+        <h1 className="heroTitle analizar">Analizar</h1>
+        <h1 className="heroTitle impulsar">Impulsar</h1>
+        <a className="heroTitle buttonShowMore descubrir" href="#about">Descubre cómo</a>
       </section>
     )
   }
