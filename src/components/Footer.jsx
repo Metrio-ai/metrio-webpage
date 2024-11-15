8f4759984bdc55645c0a09b8862a91074ea02570
sierrapablo <pablosierralorente@outlook.es>
2024-11-15 20:31:16 +0100

    add footer container

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <footer className="footerContainer">
+            <div className="footerContent">
+                <span className="footer-item"><a href="">Sobre Nosotros</a></span>
+                <span className="footer-item"><a href="">Contacto</a></span>
+                <span className="footer-item"><a href="">Política de privacidad</a></span>
+            </div>
+            <div className="footerCopyright">
+                <p>&copy; 2024 Metrio. Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
