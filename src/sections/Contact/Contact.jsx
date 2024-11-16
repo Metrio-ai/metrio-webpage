01991e169e3b2a8e8ecd75b47f5533d7f16c0c19
sierrapablo <pablosierralorente@outlook.es>
2024-11-16 06:32:17 +0100

    contact section

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <div className="contactContainer">
+        <div className="contactText">
+          <p className="contactHeader">Contacto</p>
+          <p className="contactSubheader">¿Qué proyecto tienes en mente?</p>
+          <a className="contactBtn" href="/contacto">
+            Cuéntanos
+          </a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
