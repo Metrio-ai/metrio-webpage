a11038d8e48a249f8bfa88bc7180d7f3db9da793
sierrapablo <pablosierralorente@outlook.es>
2024-11-16 04:25:28 +0100

    refactor icons & add more of them

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <section className="aboutContainer">  
             {/* Texto y tecnologías */}
             <div className="aboutContent">
-                <p className="aboutHeader">Metrio</p>
+                <p className="aboutHeader">Sobre Nosotros</p>
                 <p className="aboutDescription">
                     En Metrio creamos soluciones a medida que transforman tus datos
                     en decisiones inteligentes. Diseñamos estrategias únicas,
@@ -15,42 +15,14 @@ function About() {
                 </p>
                 {/* Lista de tecnologías */}
                 <div className="aboutTechIcons">
-                    <img
-                    className="techIcon"
-                    src="/src/assets/img/python.svg"
-                    alt="Python"
-                    />
-                    <img
-                    className="techIcon"
-                    src="/src/assets/img/nodejs.svg"
-                    alt="NodeJS"
-                    />
-                    <img
-                    className="techIcon"
-                    src="/src/assets/img/cplusplus.svg"
-                    alt="C++"
-                    />
-                    <img
-                    className="techIcon"
-                    src="/src/assets/img/postgresql.svg"
-                    alt="PostgreSQL"
-                    />
-                    <img
-                    className="techIcon"
-                    src="/src/assets/img/mongodb.svg"
-                    alt="MongoDB"
-                    />
-                    <img
-                    className="techIcon"
-                    src="/src/assets/img/react.svg"
-                    alt="React"
-                    />
-                    <img
-                    className="techIcon"
-                    src="/src/assets/img/docker.svg"
-                    alt="Docker"
-                    />
-                    <p className="aboutDescription">
+                    <img className="techIcon" src="/src/assets/img/python.svg" alt="Python" />
+                    <img className="techIcon" src="/src/assets/img/nodejs.svg" alt="NodeJS" />
+                    <img className="techIcon" src="/src/assets/img/postgresql.svg" alt="PostgreSQL" />
+                    <img className="techIcon" src="/src/assets/img/mongodb.svg" alt="MongoDB" />
+                    <img className="techIcon" src="/src/assets/img/react.svg" alt="React" />
+                    <img className="techIcon" src="/src/assets/img/docker.svg" alt="Docker" />
+                </div>
+                <p className="aboutDescription">
                     Seleccionamos cuidadosamente las herramientas 
                     y tecnologías que nos permiten 
                     ofrecer soluciones innovadoras, confiables y escalables. 
@@ -58,7 +30,14 @@ function About() {
                     robustos hasta plataformas avanzadas de almacenamiento y 
                     procesamiento, nuestra meta es 
                     garantizar resultados sobresalientes en cada proyecto.
-                    </p>
+                </p>
+                <div className="aboutTechIcons">
+                    <img className="techIcon" src="/src/assets/img/excel.svg" alt="Excel" />
+                    <img className="techIcon" src="/src/assets/img/powerbi.svg" alt="PowerBI" />
+                    <img className="techIcon" src="/src/assets/img/tableau.svg" alt="Tableau" />
+                    <img className="techIcon" src="/src/assets/img/pandas.svg" alt="Pandas" />
+                    <img className="techIcon" src="/src/assets/img/matplotlib.svg" alt="Matplotlob" />
+                    <img className="techIcon" src="/src/assets/img/tensorflow.svg" alt="TensorFlow" />
                 </div>
             </div>
         </section>
