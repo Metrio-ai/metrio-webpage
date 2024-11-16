cf0d6ff637a5fecc324d0add29a0d45ad497cf31
sierrapablo <pablosierralorente@outlook.es>
2024-11-16 08:01:39 +0100

    add section reference

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,15 +3,17 @@ import './Hero.css'
 
 function Hero() {
   
-    return (
-      <section className="heroContainer">
-        <div className="heroOverlay"></div>
-        <h1 className="heroTitle conectar">Conectar</h1>
-        <h1 className="heroTitle analizar">Analizar</h1>
-        <h1 className="heroTitle impulsar">Impulsar</h1>
-        <a className="heroTitle buttonShowMore descubrir" href="#about">Descubre cómo</a>
-      </section>
-    )
-  }
+  return (
+    <section id="hero" className="heroContainer">
+      <div className="heroOverlay"></div>
+      <h1 className="heroTitle conectar">Conectar</h1>
+      <h1 className="heroTitle analizar">Analizar</h1>
+      <h1 className="heroTitle impulsar">Impulsar</h1>
+      <a className="heroTitle buttonShowMore descubrir" href="#about">
+        Descubre cómo
+      </a>
+    </section>
+  )
+}
   
-  export default Hero
+export default Hero
