b7d24072c43b789f9ecddebc3c1122ce389a6ed4
sierrapablo <pablosierralorente@outlook.es>
2024-11-16 06:32:01 +0100

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
 import About from './sections/About/About'
+import Contact from './sections/Contact/Contact'
 import Hero from './sections/Hero/Hero'
 import './styles/App.css'
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Contact />
       <Footer />
     </div>
   )
