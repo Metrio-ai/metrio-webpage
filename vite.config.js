819a66f4e1c89965902a2164e7fc0bce3ab047da
sierrapablo <pablosierralorente@outlook.es>
2024-11-17 19:03:57 +0100

    update base path to show page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Metrio-ai.github.io/metrio-webpage" // https://<USERNAME/ORGNAME>.github.io/<REPOSITORY>
+  base: "/"
 })
