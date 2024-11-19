ef9bc2a6b3549559b36349a8f273840cc3ae553a
Pablo Sierra Lorente <pablo@metrio.es>
2024-11-19 22:28:08 +0100

    update base url and directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -6,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
