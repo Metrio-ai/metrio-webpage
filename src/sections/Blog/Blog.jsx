fb87301055000824e6e3b65b80f4f3dbe7e875a6
Pablo Sierra Lorente <pablo@metrio.es>
2024-11-19 12:58:19 +0100

    restructure main to redirect to new pages

---
+++ src/sections/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import './Blog.css'
+
+function Blog() {
+  return (
+    <div className="blogContainer">
+      <h1>Bienvenido a nuestro Blog</h1>
+      <p>Sección en construcción</p>
+    </div>
+  )
+}
+
+export default Blog
