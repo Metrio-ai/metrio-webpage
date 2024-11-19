f726c67a312ac300ed5a4df3193bcc94f09e6499
Pablo Sierra Lorente <pablo@metrio.es>
2024-11-20 00:10:16 +0100

    refactor main to use hash routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,29 @@
-/* eslint-disable no-undef */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.jsx'
+import Blog from './sections/Blog/Blog.jsx'
+import BlogPost from './sections/Blog/BlogPost.jsx'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/blog',
+    element: <Blog />
+  },
+  {
+    path: 'blog/:slug',
+    element: <BlogPost />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={window.location.hostname === "localhost" ? "" : process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router}>
+    </RouterProvider>
   </StrictMode>,
 )
