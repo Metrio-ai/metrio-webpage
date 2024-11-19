e8962fcd35363abb2ded6b3d84ee508bfeeb9cd3
Pablo Sierra Lorente <pablo@metrio.es>
2024-11-20 00:18:55 +0100

    refactor API call to get by slug

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,5 @@
-const API_URL = 'http://api.metrio.es:3000/api/posts';
+const API_URL = 'http://api.metrio.es:3000/api/posts'
 
-/**
- * Fetches all posts from the API.
- * @returns {Promise<Array>} An array of posts.
- */
 export const getPosts = async () => {
   try {
     const response = await fetch(API_URL, {
@@ -11,23 +7,18 @@ export const getPosts = async () => {
       headers: {
         'Content-Type': 'application/json'
       }
-    });
+    })
     if (!response.ok) {
-      throw new Error(`Error fetching posts: ${response.status} ${response.statusText}`);
+      throw new Error(`Error fetching posts: ${response.status} ${response.statusText}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error('Error fetching posts:', error);
-    throw error;
+    console.error('Error fetching posts:', error)
+    throw error
   }
-};
+}
 
-/**
- * Fetches a single post by its ID.
- * @param {string} id - The ID of the post to fetch.
- * @returns {Promise<Object>} The post object.
- */
 export const getPostById = async (id) => {
   try {
     const response = await fetch(`${API_URL}/${id}`, {
@@ -35,14 +26,14 @@ export const getPostById = async (id) => {
       headers: {
         'Content-Type': 'application/json'
       }
-    });
+    })
     if (!response.ok) {
-      throw new Error(`Error fetching post by ID: ${response.status} ${response.statusText}`);
+      throw new Error(`Error fetching post by ID: ${response.status} ${response.statusText}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error('Error fetching post by ID:', error);
-    throw error;
+    console.error('Error fetching post by ID:', error)
+    throw error
   }
-};
+}
