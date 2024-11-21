a2ea07d108b184d856bf6d018162fb885037dd04
Pablo Sierra Lorente <pablo@metrio.es>
2024-11-21 17:08:24 +0100

    fix postCard hrefs

--- src/sections/Blog/PostCard.jsx
+++ src/sections/Blog/PostCard.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 function PostCard ({ title, author, date, description, href }) {
   return (
-    <div className='postCard'>
-      <h3>
-        <Link to={`${href}`}>{title}</Link>
-      </h3>
-      <p className='postDate'>{date}</p>
-      <p className='postAuthor'>Autor: {author}</p>
-      <p className='postDescription'>{description}</p>
-    </div>
+    <Link to={`${href}`} className='postCard'>
+      <div>
+        <h3>{title}</h3>
+        <p className='postDate'>{date}</p>
+        <p className='postAuthor'>Autor: {author}</p>
+        <p className='postDescription'>{description}</p>
+      </div>
+    </Link>
   )
 }
 
